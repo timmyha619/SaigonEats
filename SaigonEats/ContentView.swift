7e32447df304b370b5f8113e7234f2527aa4e938
Duc <timmyha619@yahoo.com>
2022-07-31 15:52:45 +0700

    Initial Commit

---
+++ SaigonEats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaigonEats
+//
+//  Created by duc on 31/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SaigonEats/SaigonEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaigonEatsApp.swift
+//  SaigonEats
+//
+//  Created by duc on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SaigonEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
